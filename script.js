6164b135fb671788a5e1e5d832a9c5c4d7af1606
Artur-UA <kovalskartur@yahoo.com>
2021-05-06 02:14:48 +0300

    fix mistake

--- script.js
+++ script.js
@@ -2,17 +2,44 @@ var changeBg = document.getElementById("bg");
 var changeText = document.getElementById("text");
 
 function buttonFirst(){
-    changeBg.style.background = "#f8f8f8 url('./img/title_1.png') no-repeat";
+    changeBg.style.background = "#f8f8f8 url('./img/title_2.png') no-repeat";
 }
 
 function buttonTwo(){
-    changeBg.style.background =  " #f8f8f8 url('./img/title_2.png') no-repeat";
+    changeBg.style.background = "#f8f8f8 url('./img/title_1_bg.png') no-repeat";
 }
 
 function buttonThree(){
     changeBg.style.background = "#f5f5f5 no-repeat";
 }
 
-function changeTitle() {
+/* function changeTitle() {
     changeText.innerHTML = "New text!";
+}  */
+
+function fullText_1(){
+    document.getElementById("textNews_1").innerHTML="7 февраля 2016 года в 14.00 прихожане строящегося храма в честь преподобного Саввы Сторожевского встречали владыку Пантелеимона, епископа Орехово-Зуевского. Жители нашего района собрались на чин освящения закладного камня в фундамент будущего храма. Это поистине историческое событие для жителей нашего района и города в целом. Храм преподобного Саввы Сторожевского великого подвижника и ученика преподобного Сергия Радонежского, станет единственным в нашей столице.";
+}
+
+function fullText_2(){
+    document.getElementById("textNews_2").innerHTML="2 февраля 2017 года строительную площадку храма преподобного Саввы Сторожевского в Измайлово посетили член комитета Госдумы по земельным отношениям и строительству, советник Святейшего Патриарха Московского и всея Руси Кирилла, Владимир Иосифович Ресин. Вместе с ним строящийся храм посетил архимандрит Илия.  ходе визита были отмечены значительные успехи строительства и обсуждены ближайшие планы продолжения работ.";
+}
+
+function fullPhotoContent_3(){
+    document.getElementById("photoNews_3").innerHTML="29 января Святая Православная Церковь отмечает поклонение честным веригам апостола Петра.";
+}
+function fullPhotoContent_4(){
+    document.getElementById("photoNews_4").innerHTML="2 января прихожане нашего храма собрались на традиционный молебен преподобному Савве Сторожевскому.";
+}
+function fullPhotoContent_5(){
+    document.getElementById("photoNews_5").innerHTML="19 февраля в этом году совпадает с неделей о Страшном Суде. В этот день Святая Православная Церковь...";
+}
+function fullPhotoContent_6(){
+    document.getElementById("photoNews_6").innerHTML="19 февраля в этом году совпадает с неделей о Страшном Суде. В этот день Святая Православная Церковь...";
+}
+function fullPhotoContent_7(){
+    document.getElementById("photoNews_7").innerHTML="19 февраля в этом году совпадает с неделей о Страшном Суде. В этот день Святая Православная Церковь...";
+}
+function fullPhotoContent_8(){
+    document.getElementById("photoNews_8").innerHTML="19 февраля в этом году совпадает с неделей о Страшном Суде. В этот день Святая Православная Церковь...";
 }
